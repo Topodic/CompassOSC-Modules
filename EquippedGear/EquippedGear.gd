ac66187581d9df74cd7e87b889d3e4a29a76c512
Topodic <43795565+Topodic@users.noreply.github.com>
2024-05-11 10:56:45 -0700

    Tidy up by moving sending logic to its own function

--- EquippedGear/EquippedGear.gd
+++ EquippedGear/EquippedGear.gd
@@ -3,90 +3,40 @@ extends CPMOSCModule
 var should_send = true
 var since_last = 0.0
 
-var head_prev = null
-var body_prev = null
-var legs_prev = null
-var feet_prev = null
-var main_hand_prev = null
-var off_hand_prev = null
+var previous = {
+	"head": null,
+	"body": null,
+	"legs": null,
+	"feet": null,
+	"main_hand": null,
+	"off_hand": null
+}
 
-var head = null
-var body = null
-var legs = null
-var feet = null
-var main_hand = null
-var off_hand = null
+var current = {
+	"head": null,
+	"body": null,
+	"legs": null,
+	"feet": null,
+	"main_hand": null,
+	"off_hand": null
+}
 
 func _process(delta):
 	since_last += delta
 func on_message_received(address, arguments):
 	match address:
 		"/cpm/armor/helmet/id":
-			var current = arguments[0]
-			should_send = current != head or since_last >= 1.0
-			head_prev = head
-			head = current
-			if should_send:
-				if head_prev:
-					send_message("head/" + head_prev.replace(":", "/"), false)
-					send_message("head/" + get_item_name(head_prev), false)
-				send_message("head/" + head.replace(":", "/"), true)
-				send_message("head/" + get_item_name(head), true)
+			handle_part("head", arguments)
 		"/cpm/armor/chestplate/id":
-			var current = arguments[0]
-			should_send = current != body or since_last >= 1.0
-			body_prev = body
-			body = current
-			if should_send:
-				if body_prev:
-					send_message("body/" + body_prev.replace(":", "/"), false)
-					send_message("body/" + get_item_name(body_prev), false)
-				send_message("body/" + body.replace(":", "/"), true)
-				send_message("body/" + get_item_name(body), true)
+			handle_part("body", arguments)
 		"/cpm/armor/leggings/id":
-			var current = arguments[0]
-			should_send = current != legs or since_last >= 1.0
-			legs_prev = legs
-			legs = current
-			if should_send:
-				if legs_prev:
-					send_message("legs/" + legs_prev.replace(":", "/"), false)
-					send_message("legs/" + get_item_name(legs_prev), false)
-				send_message("legs/" + legs.replace(":", "/"), true)
-				send_message("legs/" + get_item_name(legs), true)
+			handle_part("legs", arguments)
 		"/cpm/armor/boots/id":
-			var current = arguments[0]
-			should_send = current != feet or since_last >= 1.0
-			feet_prev = feet
-			feet = current
-			if should_send:
-				if feet_prev:
-					send_message("feet/" + feet_prev.replace(":", "/"), false)
-					send_message("feet/" + get_item_name(feet_prev), false)
-				send_message("feet/" + feet.replace(":", "/"), true)
-				send_message("feet/" + get_item_name(feet), true)
+			handle_part("feet", arguments)
 		"/cpm/held_item/id":
-			var current = arguments[0]
-			should_send = current != main_hand or since_last >= 1.0
-			main_hand_prev = main_hand
-			main_hand = current
-			if should_send:
-				if main_hand_prev:
-					send_message("main_hand/" + main_hand_prev.replace(":", "/"), false)
-					send_message("main_hand/" + get_item_name(main_hand_prev), false)
-				send_message("main_hand/" + main_hand.replace(":", "/"), true)
-				send_message("main_hand/" + get_item_name(main_hand), true)
+			handle_part("main_hand", arguments)
 		"/cpm/off_hand/id":
-			var current = arguments[0]
-			should_send = current != off_hand or since_last >= 1.0
-			off_hand_prev = off_hand
-			off_hand = current
-			if should_send:
-				if off_hand_prev:
-					send_message("off_hand/" + off_hand_prev.replace(":", "/"), false)
-					send_message("off_hand/" + get_item_name(off_hand_prev), false)
-				send_message("off_hand/" + off_hand.replace(":", "/"), true)
-				send_message("off_hand/" + get_item_name(off_hand), true)
+			handle_part("off_hand", arguments)
 			
 		"/cpm/gameTime":
 			since_last = 0.0
@@ -96,3 +46,15 @@ func get_item_name(id : String) -> String:
 	var item_name = id.trim_prefix(source + ":")
 	
 	return item_name
+
+func handle_part(part_name : String, arguments):
+	var _cur = arguments[0]
+	should_send = _cur != current[part_name] or since_last >= 1.0
+	previous[part_name] = current[part_name]
+	current[part_name] = _cur
+	if should_send:
+		if previous[part_name]:
+			send_message(part_name + "/" + previous[part_name].replace(":", "/"), false)
+			send_message(part_name + "/" + get_item_name(previous[part_name]), false)
+		send_message(part_name + "/" + current[part_name].replace(":", "/"), true)
+		send_message(part_name + "/" + get_item_name(current[part_name]), true)
