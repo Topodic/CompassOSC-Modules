fb7f27eb3d2cdcbeeb49386a903ab92edf1d427b
Topodic <43795565+Topodic@users.noreply.github.com>
2024-05-11 10:58:08 -0700

    Add item tag support to EquippedGear module

--- EquippedGear/EquippedGear.gd
+++ EquippedGear/EquippedGear.gd
@@ -21,22 +21,81 @@ var current = {
 	"off_hand": null
 }
 
+var previous_tags = {
+	"head": [],
+	"body": [],
+	"legs": [],
+	"feet": [],
+	"main_hand": [],
+	"off_hand": []
+}
+
+var current_tags = {
+	"head": [],
+	"body": [],
+	"legs": [],
+	"feet": [],
+	"main_hand": [],
+	"off_hand": []
+}
+
+var dirty_tags = {
+	"head": false,
+	"body": false,
+	"legs": false,
+	"feet": false,
+	"main_hand": false,
+	"off_hand": false
+}
+
+var head_prev = null
+var body_prev = null
+var legs_prev = null
+var feet_prev = null
+var main_hand_prev = null
+var off_hand_prev = null
+
+var head = null
+var body = null
+var legs = null
+var feet = null
+var main_hand = null
+var off_hand = null
+
 func _process(delta):
 	since_last += delta
 func on_message_received(address, arguments):
 	match address:
 		"/cpm/armor/helmet/id":
 			handle_part("head", arguments)
+		"/cpm/armor/helmet/tags":
+			if dirty_tags["head"]:
+				handle_tags("head", arguments)
 		"/cpm/armor/chestplate/id":
 			handle_part("body", arguments)
+		"/cpm/armor/chestplate/tags":
+			if dirty_tags["body"]:
+				handle_tags("body", arguments)
 		"/cpm/armor/leggings/id":
 			handle_part("legs", arguments)
+		"/cpm/armor/leggings/tags":
+			if dirty_tags["legs"]:
+				handle_tags("legs", arguments)
 		"/cpm/armor/boots/id":
 			handle_part("feet", arguments)
+		"/cpm/armor/boots/tags":
+			if dirty_tags["feet"]:
+				handle_tags("feet", arguments)
 		"/cpm/held_item/id":
 			handle_part("main_hand", arguments)
+		"/cpm/held_item/tags":
+			if dirty_tags["main_hand"]:
+				handle_tags("main_hand", arguments)
 		"/cpm/off_hand/id":
 			handle_part("off_hand", arguments)
+		"/cpm/off_hand/tags":
+			if dirty_tags["off_hand"]:
+				handle_tags("off_hand", arguments)
 			
 		"/cpm/gameTime":
 			since_last = 0.0
@@ -53,8 +112,20 @@ func handle_part(part_name : String, arguments):
 	previous[part_name] = current[part_name]
 	current[part_name] = _cur
 	if should_send:
+		dirty_tags[part_name] = true
 		if previous[part_name]:
 			send_message(part_name + "/" + previous[part_name].replace(":", "/"), false)
 			send_message(part_name + "/" + get_item_name(previous[part_name]), false)
 		send_message(part_name + "/" + current[part_name].replace(":", "/"), true)
 		send_message(part_name + "/" + get_item_name(current[part_name]), true)
+
+func handle_tags(part_name : String, arguments):
+	dirty_tags[part_name] = false
+	previous_tags[part_name] = current_tags[part_name]
+	current_tags[part_name] = arguments
+	for tag in previous_tags[part_name]:
+		var t = tag.replace(":", "/").replace("$", "").replace("#", "")
+		send_message(part_name + "/tag/" + t, false)
+	for tag in current_tags[part_name]:
+		var t = tag.replace(":", "/").replace("$", "").replace("#", "")
+		send_message(part_name + "/tag/" + t, true)
